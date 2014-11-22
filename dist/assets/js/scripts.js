7e5d5038769655f4731d6f116b61a06f25f2612a
Bhanu Mokkala <bhanureddy@gmail.com>
2014-11-22 23:52:17 +0530

    first set of changes

--- dist/assets/js/scripts.js
+++ dist/assets/js/scripts.js
@@ -1,6 +1,14 @@
 
 jQuery(document).ready(function() {
 
+    $('.timer').fadeIn(3000);
+    $('.ccaption h2').animate({right:'-15px'}, 3);
+    $('.ccaption h2').fadeIn({queue: false, duration: 3000});
+    $('.ccaption h2').animate({right:'15px'}, 3000); 
+    $('.ccaption p').animate({left:'-15px'}, 3);
+    $('.ccaption p').fadeIn({queue: false, duration: 3000})
+    $('.ccaption p').animate({left:'15px'}, 3000);        
+
     /*
         Background slideshow
     */
@@ -8,13 +16,13 @@ jQuery(document).ready(function() {
       "assets/img/backgrounds/1.jpg"
     , "assets/img/backgrounds/2.jpg"
     , "assets/img/backgrounds/3.jpg"
-    ], {duration: 3000, fade: 750});
+    ], {duration: 2000, fade: 750});
 
     /*
         Countdown initializer
     */
     var now = new Date();
-    var countTo = 25 * 24 * 60 * 60 * 1000 + now.valueOf();
+    var countTo = 10 * 24 * 60 * 60 * 1000 + now.valueOf();
     $('.timer').countdown(countTo, function(event) {
         var $this = $(this);
         switch(event.type) {
